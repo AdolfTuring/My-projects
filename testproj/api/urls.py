087dc30219d660c42935d6848c9fe340f08feefd
Serhii Lutsuk <80625606+AdolfTuring@users.noreply.github.com>
2021-04-12 15:02:11 +0300

    Delete testproj directory

--- testproj/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from api import views
-
-urlpatterns = [
-path('list/<int:x>', views.message_list),
-path('single/<int:pk>', views.message_detail),
-]
